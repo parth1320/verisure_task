3e43950867a7910541eb25c5434af128a1b7ad4e
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2023-08-09 11:14:30 +0200

    Final Version

---
+++ script.js
@@ -0,0 +1,55 @@
+function nextStep(stepNumber, backgroundImg) {
+  //postalcode validation
+  if (stepNumber === 2) {
+    const postalCodeInput = document.getElementById("postalCode");
+
+    if (!postalCodeInput.value.match(/^\d{5}$/)) {
+      return;
+    }
+  }
+
+  // Hide the current step
+  document.getElementById("step" + (stepNumber - 1)).style.display = "none";
+  // Show the next step
+  document.getElementById("step" + stepNumber).style.display = "block";
+
+  // Update progress bar width based on step
+  const progressBar = document.querySelector(".progress-bar");
+  const width = (stepNumber - 1) * 50; // 3 steps, each taking 33.33% width
+  progressBar.style.width = width + "%";
+
+  //set the backgroung image in step 3 depend on step 2 options
+  //dynamic addition of id
+  const containerRed = document.getElementById("container-red");
+  containerRed.style.backgroundImage = `url(${backgroundImg})`;
+}
+
+const bottomImageContainer = document.getElementById("bottom-image-container");
+//Dynamically adjuct the position of the lop-level image
+function adjustTopImagePosition() {
+  const windowHeight = window.innerHeight;
+  const containerRed = document.getElementById("container-red");
+  const containerRedBottom = containerRed.getBoundingClientRect().bottom;
+  const formElements = document.querySelectorAll(".step .form-control .btn");
+
+  //postion of the last form element
+  let lastFormElementBottom = 0;
+  formElements.forEach((formElement) => {
+    const formElementBottom = formElement.getBoundingClientRect().bottom;
+    if (formElementBottom > lastFormElementBottom) {
+      lastFormElementBottom = formElementBottom;
+    }
+  });
+
+  const distance = lastFormElementBottom - containerRedBottom;
+
+  if (distance < 0) {
+    const newBottom = windowHeight - containerRedBottom + distance;
+    bottomImageContainer.style.bottom = `${newBottom}px`;
+  } else {
+    bottomImageContainer.style.bottom = "0";
+  }
+}
+
+// window.onload = adjustTopImagePosition;
+// window.onresize = adjustTopImagePosition;
---
+++ submit.php
@@ -0,0 +1,42 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //form data
+    $postalCode = $_POST["postalCode"];
+    $phoneNumber = $_POST["phoneNumber"];
+
+    $apiKey = 1234;
+
+    $data = array(
+        "api_key"=>"1234",
+        "country_code"=>'DE',
+        "web_campaign" => "Test-Funnel-build",
+        "phone" => $phoneNumber,
+        "postalcode" => $postalCode,
+    );
+
+    $jsonData = json_encode($data);
+
+    $apiUrl = "https://www.verisure.de/api/maia-pro/lead/post/";
+
+    $ch = curl_init($apiUrl);
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo 'Error: ' . curl_error($ch);
+    }
+
+    curl_close($ch);
+
+    echo $response;
+
+}
+?>
+
+
